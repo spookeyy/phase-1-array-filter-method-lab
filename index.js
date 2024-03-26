5094776679a461da0b2e65c0944f9d59c6d97081
spookeyy <pangasmeshack@gmail.com>
2024-03-26 23:07:55 +0300

    passing 1 test of 7

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, searchName){
+    if (!searchName) {
+        return [];
+    }
+
+    const loweredSearchName = searchName.toLowerCase();  //convert search name
+    const loweredDrivers = drivers.map(driver => driver.toLowerCase());
+
+    const matchingDrivers = loweredDrivers.filter(driver => driver == loweredSearchName)
+
+    return matchingDrivers;
+}
+
+let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+console.log(findMatching(drivers, "Bobby"));
