a4262ce6b163ed3c7b9b32f025293f02be299e11
spookeyy <pangasmeshack@gmail.com>
2024-03-27 17:18:03 +0300

    pass lab test

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ function findMatching(drivers, searchName){
 let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
 console.log(findMatching(drivers, "Sammy"));
 
-function fuzzyMatch(params) {
-    
-}
+function fuzzyMatch(source, testString) {
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+
+  function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+  }
